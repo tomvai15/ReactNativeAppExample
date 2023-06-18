f514041fa6142d0bb67d9d2f6ad934476b788055
tomvai15 <78442825+tomvai15@users.noreply.github.com>
2023-06-18 18:25:23 +0300

    feeat: add eslint

---
+++ src/native-app/.eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'react-native/react-native': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'overrides': [
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    'react-native',
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};
